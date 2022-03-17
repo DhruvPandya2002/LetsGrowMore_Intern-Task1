40eada27c98b6a8901d3834d755f952efb020ca7
DhruvPandya2002 <dhruvpandya2002@gmail.com>
2022-03-17 21:55:35 +0530

    first commit

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
   const input_el = document.querySelector("#input1");
   const add = document.querySelector("#add");
-  const total_el = document.querySelector("#list_in");
     add.addEventListener("click",(e)=>
     {
       e.preventDefault();
@@ -16,15 +15,30 @@
         let task_el = document.createElement("div");
         task_el.setAttribute("id","list_in");
         div3.appendChild(task_el);
-      
+
+        //ckeck functionality
+        let dun= document.createElement("input");
+        dun.type='checkbox';
+        dun.setAttribute("id","dun");
+        dun.onclick=()=>
+        {
+          
+          let edit= dun.parentElement;
+          edit.classList.toggle('completed');
+        }
+        task_el.appendChild(dun);
+       
+        //input functionality
         let redirect= document.createElement("p");
         redirect.setAttribute("id","show");
         redirect.textContent=list_el;
+        document.querySelector('#input1').value=null;
         task_el.appendChild(redirect);
-
-        let delete_el= document.createElement("p");
+        
+        //delet functionality
+        let delete_el= document.createElement("button");
         delete_el.setAttribute("id","delete");
-        delete_el.textContent="Delete";  
+        delete_el.innerHTML="Delete";  
         task_el.appendChild(delete_el);
         delete_el.onclick=()=>
         {
